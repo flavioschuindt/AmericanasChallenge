612a9506be7b4d3752a2d55d5bd01bb9f1881932
Flávio Schuindt <flavio.schuindt@gmail.com>
2012-08-15 14:12:31 -0300

    * Create a 'Challenge' module * Fix some bugs with prices format

--- desafio.py
+++ desafio.py
@@ -1,25 +1,20 @@
+# -*- coding: utf-8 -*-
 # A simple script to extract product prices from Americanas.com
 # Usage: desafio.py URL
 # It simply connect to the url using requests lib and does a parse using xpath to discover the price inside the page
-
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-import sys
-import requests
-from lxml import html,etree
+from Challenge import *
 
-def do_request(url) :
-	response = requests.get(url)
-	return html.fromstring(response.text)		
-def get_url_from_command_line() :
- 	url = ""
-        for param in sys.argv[1:2] :
-                url = param
-        return url
+challenge = Challenge()
+url = challenge.get_url_from_command_line()
+results = challenge.do_request(url).xpath("//span[@class='amount']/text()")
+unavailable = challenge.do_request(url).xpath("//div[@class='unavailProd']/text()")
+if len(unavailable) > 0:
+	print u'Produto indisponível'
+elif len(results) > 0 :
+        print "Original: '"+results[0]+"'"
+        for index,item in enumerate(results):
+        	results[index] = results[index][8:].replace(".","")
+                results[index] = results[index].replace(",",".")
+      	print "Float: "+str(map(float,results)[0])
 
-url = get_url_from_command_line()
-results = do_request(url).xpath("//span[@class='amount']/text()")
-print "Original: '"+results[0]+"'"
-for index,item in enumerate(results):
-	results[index] = results[index][8:].replace(",",".")
-print "Float: "+str(map(float,results)[0])
