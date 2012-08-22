6813f4097f871c0a2e942b5c0a2193de20f559c7
Flávio Schuindt <flavio.schuindt@gmail.com>
2012-08-22 19:51:45 -0300

    - Challenge 6 done! - Some refactoring in the code

--- desafio6.py
+++ desafio6.py
@@ -4,8 +4,8 @@ import sys; sys.path.append('modules')
 from Challenge import *
 
 challenge = Challenge()
-url = "http://hughes.sieve.com.br:9090/level4/"
-cookie = {'cade-meu-cookie':'esta aqui'}
-data = challenge.do_request(url,None,cookie)['response']
+url = "http://hughes.sieve.com.br:9090/level5/"
+location = challenge.quote_url(challenge.do_request(url,None,None)['history'][0].headers['location'])
+data = challenge.do_request(location,None,None)['response']
 results = data.xpath("//p/text()")
 print "Preço: "+str(challenge.get_float_price(results[0]))
--- modules/Challenge.py
+++ modules/Challenge.py
@@ -2,6 +2,7 @@
 import sys
 import requests
 import re
+import urllib,os
 from lxml import html
 class Challenge:
 	def do_request(self,url,headers=None,cookie=None) :
@@ -9,9 +10,11 @@ class Challenge:
 		status_and_response = {}
 		if (len(response.history) > 0):
 			#Moved to home or departament page
-			status_and_response = dict(status_code=response.history[0].status_code,response=html.fromstring(response.text),cookies=response.cookies)
+			status_and_response = dict(status_code=response.history[0].status_code,response=html.fromstring(response.text),\
+									   cookies=response.cookies,response_headers=response.headers,history=response.history)
 		else:
-			status_and_response = dict(status_code=response.status_code,response=html.fromstring(response.text),cookies=response.cookies)
+			status_and_response = dict(status_code=response.status_code,response=html.fromstring(response.text),\
+									   cookies=response.cookies,response_headers=response.headers,history=response.history)
 		return status_and_response
 	def get_url_from_command_line(self) :
  		url = ""
@@ -24,3 +27,8 @@ class Challenge:
 		price = price.replace(",",".")
 		price = float(price)
 		return price
+	def quote_url(self,url):
+		new_url = url[:-1]
+		index = new_url.rfind('/')
+		path = urllib.quote(os.path.basename(new_url))
+		return new_url[0:32] + path
